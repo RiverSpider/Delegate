7e98976bbbcda36a0105255cad0b698ecd0a727b
RiverSpider <raevskih8888@mail.ru>
2023-04-15 13:02:41 +0600

    Добавьте файлы проекта.

---
+++ Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+delegate double MathOperation(double x, double y, double z);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        MathOperation min = (x, y, z) => Math.Min(x, Math.Min(y, z));
+        MathOperation max = (x, y, z) => Math.Max(x, Math.Max(y, z));
+        MathOperation sum = (x, y, z) => x + y + z;
+        MathOperation prod = (x, y, z) => x * y * z;
+        MathOperation average = (x, y, z) => (x + y + z) / 3.0;
+
+        Console.WriteLine($"Min: {min(a, b, c)}");
+        Console.WriteLine($"Max: {max(a, b, c)}");
+        Console.WriteLine($"Sum: {sum(a, b, c)}");
+        Console.WriteLine($"Prod: {prod(a, b, c)}");
+        Console.WriteLine($"Average: {average(a, b, c)}");
+    }
+}
